c3ce33f3557845c9a63becf311f06280356469b0
Ramiro Araujo <rama.araujo@gmail.com>
2024-10-06 18:49:56 -0300

    context generator

---
+++ bin/regenerateContext.sh
@@ -0,0 +1,14 @@
+DEST_FOLDER=~/Desktop/provincias
+mkdir -p "$DEST_FOLDER"
+rm -rf "$DEST_FOLDER"/*
+
+tree /Users/ramiroaraujo/Development/provincias-y-capitales/src > "$DEST_FOLDER/src-tree.txt"
+
+# @todo add git log
+
+cp /Users/ramiroaraujo/Development/provincias-y-capitales/src/lib/game.ts "$DEST_FOLDER/"
+cp /Users/ramiroaraujo/Development/provincias-y-capitales/src/lib/provinces.ts "$DEST_FOLDER/"
+cp /Users/ramiroaraujo/Development/provincias-y-capitales/src/lib/utils.ts "$DEST_FOLDER/"
+cp /Users/ramiroaraujo/Development/provincias-y-capitales/package.json "$DEST_FOLDER/"
+
+open "$DEST_FOLDER"
