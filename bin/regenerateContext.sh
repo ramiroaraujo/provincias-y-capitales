5789c9d5a0c7b985ff6cf5ceafb794b9699db8d4
Ramiro Araujo <rama.araujo@gmail.com>
2024-10-06 20:10:58 -0300

    add more files to context gen

--- bin/regenerateContext.sh
+++ bin/regenerateContext.sh
@@ -6,6 +6,8 @@ tree /Users/ramiroaraujo/Development/provincias-y-capitales/src > "$DEST_FOLDER/
 
 # @todo add git log
 
+cp /Users/ramiroaraujo/Development/provincias-y-capitales/src/app/layout.tsx "$DEST_FOLDER/"
+cp /Users/ramiroaraujo/Development/provincias-y-capitales/src/app/page.tsx "$DEST_FOLDER/"
 cp /Users/ramiroaraujo/Development/provincias-y-capitales/src/lib/game.ts "$DEST_FOLDER/"
 cp /Users/ramiroaraujo/Development/provincias-y-capitales/src/lib/provinces.ts "$DEST_FOLDER/"
 cp /Users/ramiroaraujo/Development/provincias-y-capitales/src/lib/utils.ts "$DEST_FOLDER/"
